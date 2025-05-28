6d04b7ac63d8e4bb67344df90479e6090364f302
laugh0608 <laugh608@163.com>
2025-05-28 21:03:55 +0800

    添加项目文件

---
+++ CapeOpenCore.Class/CapeMaterialObjectBase.cs
@@ -0,0 +1,13 @@
+﻿namespace CapeOpenClassLibrary
+{
+    //[Serializable]
+    //[System.Runtime.InteropServices.ComVisible(true)]
+    ////[System.Runtime.InteropServices.ComSourceInterfaces(typeof(ICapeThermoMaterialObjectCOM))]
+    //[System.Runtime.InteropServices.ClassInterface(System.Runtime.InteropServices.ClassInterfaceType.None)]
+    //public abstract class CapeMaterialObjectBase:
+    //    CapeObjectBase,
+    //    ICapeThermoMaterialObject,
+    //    ICapeThermoMaterialObjectCOM
+    //{
+    //}
+}
