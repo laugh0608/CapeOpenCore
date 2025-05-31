e530ab219240eb421d5ca3f287de4f6cb224281b
laugh0608 <laugh608@163.com>
2025-05-31 17:35:51 +0800

    完成重构ArrayParameter

--- CapeOpenCore.Class/ArrayParameter.cs
+++ CapeOpenCore.Class/ArrayParameter.cs
@@ -1,198 +1,126 @@
-﻿using System;
+﻿/*
+ * DaBaiLuoBo
+ * 2025.05.31
+ */
+
+using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 
-namespace CapeOpenCore.Class
+namespace CapeOpenCore.Class;
+
+// internal class ArrayParameter { }
+
+/// <summary>CAPE-OPEN 中使用的数组值参数的包装器<see cref="ParameterCollection">参数集合</see>。</summary>
+/// <remarks>Wraps a CAPE-OPEN array-valued parameter for use in a CAPE-OPEN <see cref="ParameterCollection">parameter collection</see>.</remarks>
+[Serializable,ComVisible(true)]
+[ComSourceInterfaces(typeof(IRealParameterSpecEvents))]
+[Guid("277E2E39-70E7-4FBA-89C9-2A19B9D5E576")]  // ICapeThermoMaterialObject_IID
+[ClassInterface(ClassInterfaceType.None)]
+internal class ArrayParameterWrapper : CapeParameter,
+    ICapeParameter, ICapeParameterSpec, ICapeArrayParameterSpec
 {
-    internal class ArrayParameter
+    [NonSerialized]
+    private ICapeParameter _mParameter;
+    
+    /// <summary>为基于 COM 的数组值参数类创建一个封装类的新实例。</summary>
+    /// <remarks>The COM-based array parameter is wrapped and exposed to .NET-based PME and PMCs.</remarks>
+    /// <param name="parameter">The COM-based array parameter to be wrapped.</param>
+    public ArrayParameterWrapper(ICapeParameter parameter) : base(
+            ((ICapeIdentification)parameter).ComponentName, 
+            ((ICapeIdentification)parameter).ComponentDescription, parameter.Mode)
     {
+        _mParameter = parameter;
     }
 
-
-    /// <summary>
-    /// Wrapper for an array-valued parameter for use in a CAPE-OPEN <see cref="ParameterCollection">parameter collection</see>.
-    /// </summary>
-    /// <remarks>
-    /// Wraps a CAPE-OPEN array-valued parameter for use in a CAPE-OPEN <see cref="ParameterCollection">parameter collection</see>.
-    /// </remarks>
-    [Serializable]
-    [ComSourceInterfaces(typeof(IRealParameterSpecEvents))]
-    [ComVisible(true)]
-    [Guid("277E2E39-70E7-4FBA-89C9-2A19B9D5E576")]  //ICapeThermoMaterialObject_IID)
-    [ClassInterface(ClassInterfaceType.None)]
-    class ArrayParameterWrapper : CapeParameter,
-        ICapeParameter,
-        ICapeParameterSpec,
-        ICapeArrayParameterSpec
+    // ICloneable
+    /// <summary>创建参数的副本。两个副本均引用同一基于 COM 的数组参数。</summary>
+    /// <remarks><para>The clone method is used to create a copy of the parameter. Both the original object and 
+    /// the clone wrap the same instance of the wrapped parameter.</para></remarks>
+    /// <returns>A copy of the current parameter.</returns>
+    public override object Clone()
     {
+        return new ArrayParameterWrapper(_mParameter);
+    }
 
-        [NonSerialized]
-        private ICapeParameter m_parameter;
+    /// <summary>UNDEFINED 用于属性参数的验证，将当前参数值与参数规范进行比对。</summary>        
+    /// <remarks>The wrapped parameter validates itself against its internal validation criteria.</remarks>
+    /// <returns>True if the parameter is valid, false if not valid.</returns>
+    /// <param name="message">Reference to a string that will contain a message regarding the validation of the parameter.</param>
+    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the prop's argument.</exception>
+    public override bool Validate(ref string message)
+    {
+        var valStatus = _mParameter.ValStatus;
+        var retVal = _mParameter.Validate(message);
+        // var args = new ParameterValidatedEventArgs(ComponentName, message, ValStatus, _mParameter.ValStatus);
+        var args = new ParameterValidatedEventArgs(ComponentName, message, ValStatus, valStatus);
+        OnParameterValidated(args);
+        NotifyPropertyChanged("ValStatus");
+        return retVal;
+    }
 
-        /// <summary>
-        /// Creates a new instance of a wrapper class for COM-based array-valued parameter class. 
-        /// </summary>
-        /// <remarks>
-        /// The COM-based array parameter is wrapped and exposed to .NET-based PME and PMCs.
-        /// </remarks>
-        /// <param name = "parameter">The COM-based array parameter to be wrapped.</param>
-        public ArrayParameterWrapper(ICapeParameter parameter)
-            : base(((ICapeIdentification)parameter).ComponentName, ((ICapeIdentification)parameter).ComponentDescription, parameter.Mode)
-        {
-            m_parameter = parameter;
-        }
+    /// <summary>将参数的值设置为其默认值。</summary>
+    /// <remarks>This method sets the parameter's value to the default value.</remarks>
+    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    public override void Reset()
+    {
+        var args = new ParameterResetEventArgs(ComponentName);
+        _mParameter.Reset();
+        NotifyPropertyChanged("Value");
+        OnParameterReset(args);
+    }
 
-        // ICloneable
-        /// <summary>
-        /// Creates a copy of the parameter. Both copies refer to the same COM-based array parameter.
-        /// </summary>
-        /// <remarks><para>The clone method is used to create a copy of the parameter. Both the original object and 
-        /// the clone wrap the same instance of the wrapped parameter.</para>
-        /// </remarks>
-        /// <returns>A copy of the current parameter.</returns>
-        override public Object Clone()
-        {
-            return new ArrayParameterWrapper(m_parameter);
-        }
+    // ICapeParameterSpec
+    /// <summary>获取参数的类型。</summary>
+    /// <remarks>Gets the <see cref="CapeParamType"/> of the parameter.</remarks>
+    /// <value>The parameter type. </value>
+    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    [Browsable(false)]
+    [Category("ICapeParameterSpec")]
+    public override CapeParamType Type => CapeParamType.CAPE_ARRAY;
 
-        /// <summary>
-        /// Validates the current value of the parameter against the specification of the parameter. 
-        /// </summary>        
-        /// <remarks>
-        /// The wrapped parameter validates iteself against its internal valication criteria.
-        /// </remarks>
-        /// <returns>
-        /// True if the parameter is valid, false if not valid.
-        /// </returns>
-        /// <param name = "message">Reference to a string that will conain a message regarding the validation of the parameter.</param>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the props argument.</exception>
-        public override bool Validate(ref String message)
-        {
-            ParameterValidatedEventArgs args;
-            CapeValidationStatus valStatus = m_parameter.ValStatus;
-            bool retval = m_parameter.Validate(message);
-            args = new ParameterValidatedEventArgs(ComponentName, message, ValStatus, m_parameter.ValStatus);
-            OnParameterValidated(args);
-            NotifyPropertyChanged("ValStatus");
-            return retval;
-        }
+    // ICapeArrayParameterSpec
+    /// <summary>获取参数规格数组。</summary>
+    /// <remarks>Gets an array of the specifications of each of the items in the value of a parameter. The Get method 
+    /// returns an array of interfaces to the correct specification type (<see cref="ICapeRealParameterSpec"/>, 
+    /// <see cref="ICapeOptionParameterSpec"/>, <see cref="ICapeIntegerParameterSpec"/>, or <see cref="ICapeBooleanParameterSpec"/>).
+    /// Note that it is also possible, for example, to configure an array of arrays, which would a 
+    /// similar but not identical concept to a two-dimensional matrix.</remarks>
+    /// <value>An array of parameter specifications. </value>
+    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    [Browsable(false)]
+    object[] ICapeArrayParameterSpec.ItemsSpecifications => ((ICapeArrayParameterSpec)_mParameter.Specification).ItemsSpecifications;
 
-        /// <summary>
-        /// Sets the value of the parameter to its default value.
-        /// </summary>
-        /// <remarks>
-        ///  This method sets the parameter's value to the default value.
-        /// </remarks>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        public override void Reset()
-        {
-            ParameterResetEventArgs args = new ParameterResetEventArgs(ComponentName);
-            m_parameter.Reset();
-            NotifyPropertyChanged("Value");
-            OnParameterReset(args);
-        }
+    /// <summary>获取参数中数组值的维数。</summary>
+    /// <remarks>The number of dimensions of the array value in the parameter.</remarks>
+    /// <value>The number of dimensions of the array value in the parameter.</value>
+    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    [Category("Parameter Specification")]
+    int ICapeArrayParameterSpec.NumDimensions => ((ICapeArrayParameterSpec)_mParameter.Specification).NumDimensions;
 
-        // ICapeParameterSpec
-        // ICapeParameterSpec
-        /// <summary>
-        /// Gets the type of the parameter. 
-        /// </summary>
-        /// <remarks>
-        /// Gets the <see cref = "CapeParamType"/> of the parameter.
-        /// </remarks>
-        /// <value>The parameter type. </value>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
-        [Browsable(false)]
-        [Category("ICapeParameterSpec")]
-        public override CapeParamType Type
-        {
-            get
-            {
-                return CapeParamType.CAPE_ARRAY;
-            }
-        }
+    /// <summary>获取数组各维度的大小。</summary>
+    /// <remarks>Gets the size of each one of the dimensions of the array.</remarks>			
+    /// <value>An integer array containing the size of each one of the dimensions of the array.</value>
+    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    [Category("Parameter Specification")]
+    int[] ICapeArrayParameterSpec.Size => ((ICapeArrayParameterSpec)_mParameter.Specification).Size;
 
-        //ICapeArrayParameterSpec
-
-        /// <summary>
-        /// Gets an array of parameter specifications.
-        /// </summary>
-        /// <remarks>
-        /// Gets an array of the specifications of each of the items in the value of a parameter. The Get method 
-        /// returns an array of interfaces to the correct specification type (<see cref=" ICapeRealParameterSpec"/>, 
-        /// <see cref=" ICapeOptionParameterSpec"/>, <see cref="ICapeIntegerParameterSpec"/>, or <see cref="ICapeBooleanParameterSpec"/>).
-        /// Note that it is also possible, for example, to configure an array of arrays, which would a 
-        /// similar but not identical concept to a two-dimensional matrix.
-        /// </remarks>
-        /// <value>An array of parameter specifications. </value>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
-        [Browsable(false)]
-        object[] ICapeArrayParameterSpec.ItemsSpecifications
-        {
-            get
-            {
-                return ((ICapeArrayParameterSpec)m_parameter.Specification).ItemsSpecifications;
-            }
-        }
-
-        /// <summary>
-        /// Gets the number of dimensions of the array value in the parameter.
-        /// </summary>
-        /// <remarks>
-        /// The number of dimensions of the array value in the parameter.
-        /// </remarks>
-        /// <value>The number of dimensions of the array value in the parameter. </value>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
-        [Category("Parameter Specification")]
-        int ICapeArrayParameterSpec.NumDimensions
-        {
-            get
-            {
-                return ((ICapeArrayParameterSpec)m_parameter.Specification).NumDimensions;
-            }
-        }
-
-        /// <summary>
-        /// Gets the size of each one of the dimensions of the array.
-        /// </summary>
-        /// <remarks>
-        /// Gets the size of each one of the dimensions of the array.
-        /// </remarks>			
-        /// <value>An integer array containing the size of each one of the dimensions of the array.</value>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
-        [Category("Parameter Specification")]
-        int[] ICapeArrayParameterSpec.Size
-        {
-            get
-            {
-                return ((ICapeArrayParameterSpec)m_parameter.Specification).Size;
-            }
-        }
-
-        /// <summary>
-        /// Determines whether a value is valid for the wrapped parameter.
-        /// </summary>
-        /// <remarks>
-        /// <para>Validates an array against the parameter's specification. It returns a flag to indicate the success or 
-        /// failure of the validation together with a text message which can be used to convey the reasoning to 
-        /// the client/user.</para>
-        /// <para>The wrapped parameter validates the value against its internal validation criteria.</para>
-        /// </remarks>
-        /// <returns>
-        /// True if the parameter is valid, false if not valid.
-        /// </returns>
-        /// <param name = "value">The value to be checked.</param>
-        /// <param name = "messages">Reference to a string that will conain a message regarding the validation of the parameter.</param>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the props argument.</exception>
-        object ICapeArrayParameterSpec.Validate(object value, ref string[] messages)
-        {
-            return ((ICapeArrayParameterSpec)m_parameter.Specification).Validate(value, ref messages);
-        }
+    /// <summary>确定封装参数的值是否有效。</summary>
+    /// <remarks><para>Validates an array against the parameter's specification. It returns a flag to indicate the success or 
+    /// failure of the validation together with a text message which can be used to convey the reasoning to the client/user.</para>
+    /// <para>The wrapped parameter validates the value against its internal validation criteria.</para></remarks>
+    /// <returns>True if the parameter is valid, false if not valid.</returns>
+    /// <param name="pValue">The value to be checked.</param>
+    /// <param name="messages">Reference to a string that will contain a message regarding the validation of the parameter.</param>
+    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the prop's argument.</exception>
+    object ICapeArrayParameterSpec.Validate(object pValue, ref string[] messages)
+    {
+        return ((ICapeArrayParameterSpec)_mParameter.Specification).Validate(pValue, ref messages);
     }
 }
