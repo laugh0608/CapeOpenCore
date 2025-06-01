bf9b942ea994014d5e9bcda677d45328e046ec28
laugh0608 <laugh608@163.com>
2025-06-01 16:54:55 +0800

    补充完全注释翻译

--- CapeOpenCore.Class/ArrayParameter.cs
+++ CapeOpenCore.Class/ArrayParameter.cs
@@ -11,21 +11,20 @@ namespace CapeOpenCore.Class;
 
 // internal class ArrayParameter { }
 
-/// <summary>CAPE-OPEN 中使用的数组值参数的包装器<see cref="ParameterCollection">参数集合</see>。</summary>
-/// <remarks>Wraps a CAPE-OPEN array-valued parameter for use in a CAPE-OPEN <see cref="ParameterCollection">parameter collection</see>.</remarks>
+/// <summary>CAPE-OPEN 中使用的数组值参数的包装器 <see cref="ParameterCollection"/> 参数集合。</summary>
+/// <remarks>将 CAPE-OPEN 数组值参数包裹起来，以便在 CAPE-OPEN 参数集合 <see cref="ParameterCollection"/> 中使用。</remarks>
 [Serializable,ComVisible(true)]
 [ComSourceInterfaces(typeof(IRealParameterSpecEvents))]
 [Guid("277E2E39-70E7-4FBA-89C9-2A19B9D5E576")]  // ICapeThermoMaterialObject_IID
 [ClassInterface(ClassInterfaceType.None)]
-internal class ArrayParameterWrapper : CapeParameter,
-    ICapeParameter, ICapeParameterSpec, ICapeArrayParameterSpec
+internal class ArrayParameterWrapper : CapeParameter, ICapeParameter, ICapeParameterSpec, ICapeArrayParameterSpec
 {
     [NonSerialized]
     private ICapeParameter _mParameter;
     
     /// <summary>为基于 COM 的数组值参数类创建一个封装类的新实例。</summary>
-    /// <remarks>The COM-based array parameter is wrapped and exposed to .NET-based PME and PMCs.</remarks>
-    /// <param name="parameter">The COM-based array parameter to be wrapped.</param>
+    /// <remarks>基于 COM 的数组参数被封装并暴露给基于 .NET 的 PME 和 PMC。</remarks>
+    /// <param name="parameter">要封装的基于 COM 的数组参数。</param>
     public ArrayParameterWrapper(ICapeParameter parameter) : base(
             ((ICapeIdentification)parameter).ComponentName, 
             ((ICapeIdentification)parameter).ComponentDescription, parameter.Mode)
@@ -35,20 +34,19 @@ internal class ArrayParameterWrapper : CapeParameter,
 
     // ICloneable
     /// <summary>创建参数的副本。两个副本均引用同一基于 COM 的数组参数。</summary>
-    /// <remarks><para>The clone method is used to create a copy of the parameter. Both the original object and 
-    /// the clone wrap the same instance of the wrapped parameter.</para></remarks>
-    /// <returns>A copy of the current parameter.</returns>
+    /// <remarks>克隆方法用于创建参数的副本。原始对象和克隆对象都包裹相同的包裹参数实例。</remarks>
+    /// <returns>当前参数的副本。</returns>
     public override object Clone()
     {
         return new ArrayParameterWrapper(_mParameter);
     }
 
     /// <summary>UNDEFINED 用于属性参数的验证，将当前参数值与参数规范进行比对。</summary>        
-    /// <remarks>The wrapped parameter validates itself against its internal validation criteria.</remarks>
-    /// <returns>True if the parameter is valid, false if not valid.</returns>
-    /// <param name="message">Reference to a string that will contain a message regarding the validation of the parameter.</param>
-    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
-    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the prop's argument.</exception>
+    /// <remarks>包装后的参数根据其内部验证标准进行验证。</remarks>
+    /// <returns>如果参数有效，则为 true；如果无效，则为 false。</returns>
+    /// <param name="message">引用一个字符串，该字符串将包含关于参数验证的消息。</param>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不合适时将引发的错误。</exception>
+    /// <exception cref="ECapeInvalidArgument">当传递的参数值无效时使用，例如未识别的复合标识符或道具参数的 UNDEFINED。</exception>
     public override bool Validate(ref string message)
     {
         var valStatus = _mParameter.ValStatus;
@@ -61,8 +59,8 @@ internal class ArrayParameterWrapper : CapeParameter,
     }
 
     /// <summary>将参数的值设置为其默认值。</summary>
-    /// <remarks>This method sets the parameter's value to the default value.</remarks>
-    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
+    /// <remarks>该方法将参数值设置为默认值。</remarks>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不合适时将引发的错误。</exception>
     public override void Reset()
     {
         var args = new ParameterResetEventArgs(ComponentName);
@@ -73,52 +71,49 @@ internal class ArrayParameterWrapper : CapeParameter,
 
     // ICapeParameterSpec
     /// <summary>获取参数的类型。</summary>
-    /// <remarks>Gets the <see cref="CapeParamType"/> of the parameter.</remarks>
-    /// <value>The parameter type. </value>
-    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
-    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    /// <remarks>获取参数的 <see cref="CapeParamType"/>。</remarks>
+    /// <value>参数的类型。</value>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不合适时将引发的错误。</exception>
+    /// <exception cref="ECapeInvalidArgument">当传递的参数值无效时使用。</exception>
     [Browsable(false)]
     [Category("ICapeParameterSpec")]
     public override CapeParamType Type => CapeParamType.CAPE_ARRAY;
 
     // ICapeArrayParameterSpec
     /// <summary>获取参数规格数组。</summary>
-    /// <remarks>Gets an array of the specifications of each of the items in the value of a parameter. The Get method 
-    /// returns an array of interfaces to the correct specification type (<see cref="ICapeRealParameterSpec"/>, 
-    /// <see cref="ICapeOptionParameterSpec"/>, <see cref="ICapeIntegerParameterSpec"/>, or <see cref="ICapeBooleanParameterSpec"/>).
-    /// Note that it is also possible, for example, to configure an array of arrays, which would a 
-    /// similar but not identical concept to a two-dimensional matrix.</remarks>
-    /// <value>An array of parameter specifications. </value>
-    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
-    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    /// <remarks><para>获取参数值中每个项目的规格的数组。Get 方法返回正确规格类型（<see cref="ICapeRealParameterSpec"/>, <see cref="ICapeOptionParameterSpec"/>,
+    /// <see cref= "ICapeIntegerParameterSpec"/>， 或 <see cref="ICapeBooleanParameterSpec"/>）的接口数组。</para>
+    /// <para>注意，例如，也可以配置一个数组，该数组类似于二维矩阵，但不完全相同。</para></remarks>
+    /// <value>参数规格数组。</value>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不合适时将引发的错误。</exception>
+    /// <exception cref="ECapeInvalidArgument">当传递的参数值无效时使用。</exception>
     [Browsable(false)]
     object[] ICapeArrayParameterSpec.ItemsSpecifications => ((ICapeArrayParameterSpec)_mParameter.Specification).ItemsSpecifications;
 
     /// <summary>获取参数中数组值的维数。</summary>
-    /// <remarks>The number of dimensions of the array value in the parameter.</remarks>
-    /// <value>The number of dimensions of the array value in the parameter.</value>
-    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
-    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    /// <remarks>参数中数组值的维数。</remarks>
+    /// <value>参数中数组值的维数。</value>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不合适时将引发的错误。</exception>
+    /// <exception cref="ECapeInvalidArgument">当传递的参数值无效时使用。</exception>
     [Category("Parameter Specification")]
     int ICapeArrayParameterSpec.NumDimensions => ((ICapeArrayParameterSpec)_mParameter.Specification).NumDimensions;
 
     /// <summary>获取数组各维度的大小。</summary>
-    /// <remarks>Gets the size of each one of the dimensions of the array.</remarks>			
-    /// <value>An integer array containing the size of each one of the dimensions of the array.</value>
-    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
-    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed.</exception>
+    /// <remarks>获取数组各维度的大小。</remarks>			
+    /// <value>整数数组，包含数组各维度的大小。</value>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不合适时将引发的错误。</exception>
+    /// <exception cref="ECapeInvalidArgument">当传递的参数值无效时使用。</exception>
     [Category("Parameter Specification")]
     int[] ICapeArrayParameterSpec.Size => ((ICapeArrayParameterSpec)_mParameter.Specification).Size;
 
     /// <summary>确定封装参数的值是否有效。</summary>
-    /// <remarks><para>Validates an array against the parameter's specification. It returns a flag to indicate the success or 
-    /// failure of the validation together with a text message which can be used to convey the reasoning to the client/user.</para>
-    /// <para>The wrapped parameter validates the value against its internal validation criteria.</para></remarks>
-    /// <returns>True if the parameter is valid, false if not valid.</returns>
-    /// <param name="pValue">The value to be checked.</param>
-    /// <param name="messages">Reference to a string that will contain a message regarding the validation of the parameter.</param>
-    /// <exception cref="ECapeUnknown">The error to be raised when other error(s), specified for this operation, are not suitable.</exception>
-    /// <exception cref="ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the prop's argument.</exception>
+    /// <remarks><para>根据参数说明验证数组。它会返回一个标志来表示验证成功或  验证成功或失败的标志，以及一条文本信息，该信息可用于向客户端/用户传达验证理由。</para>
+    /// <para>封装参数根据其内部验证标准对值进行验证。</para></remarks>
+    /// <returns>如果参数有效，则为 true；如果无效，则为 false。</returns>
+    /// <param name="pValue">要检查的值。</param>
+    /// <param name="messages">指向字符串的引用，该字符串将包含与参数验证相关的信息。</param>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不合适时将引发的错误。</exception>
+    /// <exception cref="ECapeInvalidArgument">当传递的参数值无效时使用，例如未识别的复合标识符或道具参数的 UNDEFINED。</exception>
     object ICapeArrayParameterSpec.Validate(object pValue, ref string[] messages)
     {
         return ((ICapeArrayParameterSpec)_mParameter.Specification).Validate(pValue, ref messages);
