9e98e5d3bb9671ce270204c6a3cf5d73f4067a13
laugh0608 <laugh608@163.com>
2025-06-07 11:00:19 +0800

    完成CapeThermoSystem翻译与重构

--- CapeOpenCore.Class/CapeThermoSystem.cs
+++ CapeOpenCore.Class/CapeThermoSystem.cs
@@ -1,104 +1,68 @@
-﻿using System;
+﻿/*
+ * 原作者：wbarret1 (https://github.com/wbarret1/CapeOpen)
+ * 重构 & 翻译：DaBaiLuoBo
+ * 帮助社区：CEPD@BBS (https://bbs.imbhj.com)
+ * 重构时间：2025.06.07
+ */
 
-namespace CapeOpenCore.Class
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace CapeOpenCore.Class;
+
+// 接口的类型定义，用于后续使用 typedef LPDISPATCH CapeThermoEquilibriumServerInterface;
+/// <summary>一个实现 <see cref="ICapeThermoSystem"/> 接口的类，并提供对当前计算机上可用的基于 COM 和 .Net 的物性（属性翻译为物性，下同）包的访问。</summary>
+/// <remarks>此类提供所有与 COM 注册的所有类物性包以及在全局程序集缓存中包含的所有基于 .NET 的物性包的列表。</remarks>
+[ComVisible(true)]
+[Guid("B5483FD2-E8AB-4ba4-9EA6-53BBDB77CE81")]
+[Description("CapeThermoSystem Wrapper")]
+[ClassInterface(ClassInterfaceType.None)]
+public abstract class CapeThermoSystem : CapeIdentification, ICapeThermoSystem, ICapeThermoSystemCOM
 {
-
-    // Type definition of the interface for subsequent use
-    //typedef LPDISPATCH CapeThermoEquilibriumServerInterface;
-    /// <summary>
-    /// A class that implements the <see cref ="ICapeThermoSystem">ICapeThermoSystem</see> interface and provides access 
-    /// to COM and .Net-based property packages available on the current computer.
-    /// </summary>
-    /// <remarks>
-    /// <para>This class provides a list of all
-    /// classes Property Packages registered with COM and all .Net-based property packages that are contained in the Global Assembly Cache.</para>
-    /// </remarks>
-    [System.Runtime.InteropServices.ComVisibleAttribute(true)]
-    [System.Runtime.InteropServices.GuidAttribute("B5483FD2-E8AB-4ba4-9EA6-53BBDB77CE81")]
-    [System.ComponentModel.DescriptionAttribute("CapeThermoSystem Wrapper")]
-    [System.Runtime.InteropServices.ClassInterface(System.Runtime.InteropServices.ClassInterfaceType.None)]
-    public abstract class CapeThermoSystem : CapeIdentification, ICapeThermoSystem, ICapeThermoSystemCOM
+    /// <summary>获取所有可用物性包。</summary>
+    /// <remarks>返回一个包含热力学系统支持的物性包名称的字符串数组。</remarks>
+    /// <returns>返回的受支持物性包集。包含从 COM 对象序列化的字符串数组的 System.Object 对象。</returns>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不适用时，应触发的错误。</exception>
+    /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
+    /// <exception cref = "ECapeNoImpl">ECapeNoImpl</exception>
+    object ICapeThermoSystemCOM.GetPropertyPackages()
     {
+        return GetPropertyPackages();
+    }
 
-        /// <summary>
-        /// Get the list of available property packages
-        /// </summary>
-        /// <remarks>
-        /// Returns StringArray of property pacakge names supported by the thermo system.
-        /// </remarks>
-        /// <returns>
-        /// The returned set of supported property packages.
-        /// A System.Object containing a String array marshalled from a COM Object.
-        /// </returns>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
-        /// <exception cref = "ECapeNoImpl">ECapeNoImpl</exception>
-        object ICapeThermoSystemCOM.GetPropertyPackages()
-        {
-            return this.GetPropertyPackages();
-        }
+    /// <summary>解决特定的物性包。</summary>
+    /// <remarks>将引用的物性包解析为物性包接口。</remarks>
+    /// <returns>物性包接口。</returns>
+    /// <param name = "propertyPackage">待解决的物性包。</param>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不适用时，应触发的错误。</exception>
+    /// <exception cref = "ECapeInvalidArgument">当传递无效的参数值时使用，例如，未识别的复合标识符或属性参数为未定义。</exception>
+    /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
+    object ICapeThermoSystemCOM.ResolvePropertyPackage(string propertyPackage)
+    {
+        return ResolvePropertyPackage(propertyPackage);
+    }
 
-        /// <summary>
-        /// Resolve a particular property package
-        /// </summary>
-        /// <remarks>
-        /// Resolves referenced property package to a property package interface.
-        /// </remarks>
-        /// <returns>
-        /// The Property Package Interface.
-        /// </returns>
-        /// <param name = "propertyPackage">
-        /// The property package to be resolved.
-        /// </param>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the props argument.</exception>
-        /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
-        Object ICapeThermoSystemCOM.ResolvePropertyPackage(String propertyPackage)
-        {
-            return this.ResolvePropertyPackage(propertyPackage);
-        }
+    // Process Modeling Component 简称 PMC，为过程模拟组件，简单理解为单元模块，Process Modeling Environment 简称 PME，为过程模拟环境。
+    /// <summary>创建一个名为 CapeThermoSystem 的类实例，并使用提供的名称和描述。</summary>
+    /// <remarks>可以使用此构造函数指定热力学系统的特定名称和描述。</remarks>
+    /// <param name = "name">PMC 的名称。</param>
+    /// <param name = "description">PMC 的描述。</param>
+    protected CapeThermoSystem(string name, string description) : base(name, description) { }
 
+    /// <summary>获取可用物性包列表。</summary>
+    /// <remarks>返回由热系统支持的物性包名称的字符串数组。</remarks>
+    /// <returns>返回的受支持物性包集合。</returns>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不适用时，应触发的错误。</exception>
+    /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
+    /// <exception cref = "ECapeNoImpl">ECapeNoImpl</exception>
+    public abstract string[] GetPropertyPackages();
 
-        /// <summary>Creates an instance of the CapeThermoSystem class with the name and description provided.</summary>
-        /// <remarks>You can use this constructor to specify a 
-        /// specific the name and description of the thermo system.
-        /// </remarks>
-        /// <param name = "name">The name of the PMC.</param>
-        /// <param name = "description">The description of the PMC.</param>
-        public CapeThermoSystem(String name, String description)
-            : base(name, description)
-        {
-        }
-
-        /// <summary>
-        /// Get the list of available property packages
-        /// </summary>
-        /// <remarks>
-        /// Returns StringArray of property pacakge names supported by the thermo system.
-        /// </remarks>
-        /// <returns>
-        /// The returned set of supported property packages.
-        /// </returns>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
-        /// <exception cref = "ECapeNoImpl">ECapeNoImpl</exception>
-        public abstract String[] GetPropertyPackages();
-
-        /// <summary>
-        /// Resolve a particular property package
-        /// </summary>
-        /// <remarks>
-        /// Resolves referenced property package to a property package interface.
-        /// </remarks>
-        /// <returns>
-        /// The Property Package Interface.
-        /// </returns>
-        /// <param name = "propertyPackage">
-        /// The property package to be resolved.
-        /// </param>
-        /// <exception cref ="ECapeUnknown">The error to be raised when other error(s),  specified for this operation, are not suitable.</exception>
-        /// <exception cref = "ECapeInvalidArgument">To be used when an invalid argument value is passed, for example, an unrecognised Compound identifier or UNDEFINED for the props argument.</exception>
-        /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
-        public abstract ICapeThermoPropertyPackage ResolvePropertyPackage(String propertyPackage);
-    };
+    /// <summary>解决特定的物性包。</summary>
+    /// <remarks>将引用的物性包解析为物性包接口。</remarks>
+    /// <returns>物性包接口。</returns>
+    /// <param name = "propertyPackage">待解决的物性包。</param>
+    /// <exception cref="ECapeUnknown">当为该操作指定的其他错误不适用时，应触发的错误。</exception>
+    /// <exception cref = "ECapeInvalidArgument">当传递无效的参数值时使用，例如，未识别的复合标识符或属性参数为未定义。</exception>
+    /// <exception cref = "ECapeFailedInitialisation">ECapeFailedInitialisation</exception>
+    public abstract ICapeThermoPropertyPackage ResolvePropertyPackage(string propertyPackage);
 }
